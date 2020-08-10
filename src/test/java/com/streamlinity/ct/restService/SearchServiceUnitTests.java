26998e7271568fdfd6c89c9799d7d37a6b896b0b
Sampath Palreddy <PalreddySampath@gmail.com>
2020-08-10 18:06:21 -0400

    removed unused import

--- src/test/java/com/streamlinity/ct/restService/SearchServiceUnitTests.java
+++ src/test/java/com/streamlinity/ct/restService/SearchServiceUnitTests.java
@@ -9,7 +9,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.rules.SpringClassRule;
